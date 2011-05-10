3da70de989b719a9c9ae87963a49b08e26ac4888
Sean McCullough <mcculloughsean@gmail.com>
2011-05-10 11:24:12 -0500

    New app demo

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7'
+
+gem 'sqlite3'
+group :development, :test do
+  gem 'web-app-theme', '>= 0.6.2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webthemetest::Application.config.secret_token = '1a4c095ee5cf841bebfc530794cc3afc31fd3ecd7820a78068fb8c945c77ac3a1a53a08e5c9d32cf4b667219b4e2c6ad8c6c09aafc54ace01a3275b00d37c3c6'
